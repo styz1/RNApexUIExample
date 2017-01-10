addf48774ab439d9617973911805ce357a0fb8b2
林金义 <linjinyi@apexsoft.com.cn>
2017-01-10 15:40:26 +0800

    项目初始化

---
+++ android/app/src/main/java/com/rnapexuiexample/MainActivity.java
@@ -0,0 +1,15 @@
+package com.rnapexuiexample;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+    /**
+     * Returns the name of the main component registered from JavaScript.
+     * This is used to schedule rendering of the component.
+     */
+    @Override
+    protected String getMainComponentName() {
+        return "RNApexUIExample";
+    }
+}
