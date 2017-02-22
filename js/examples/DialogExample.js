9909cdf39c6c53a9ad884a7ac67a2b783ee7aaee
林金义 <linjinyi@apexsoft.com.cn>
2017-02-22 10:22:26 +0800

    Dialog新增props: scrollToKeyboardOnFocus, 在输入框在获取焦点时，会自动滚动Dialog到键盘之上

--- js/examples/DialogExample.js
+++ js/examples/DialogExample.js
@@ -2,7 +2,7 @@
 'use strict';
 
 import React, {Component, PropTypes} from 'react';
-import ReactNative, {View, Text} from 'react-native';
+import ReactNative, {View, Text, TextInput} from 'react-native';
 import {Dialog, Button} from 'react-native-apex-ui';
 
 class DialogExample extends Component {
@@ -48,6 +48,48 @@ class DialogExample extends Component {
 	}
 }
 
+class ScrollToKeyboardExample extends Component {
+	state = {
+		open: false,
+	};
+
+	handleOpen = () => {
+		this.setState({open: true});
+	};
+
+	handleClose = () => {
+		this.setState({open: false});
+	};
+
+	render() {
+		return (
+			<View>
+				<Button 
+					caption='BUTTON'
+					style={styles.showButton}
+					onPress={this.handleOpen}
+				/>
+
+				<Dialog
+					title='标题'
+					scrollToKeyboardOnFocus={true}
+					open={this.state.open}
+					onRequestClose={this.handleClose}
+				>
+					<Text style={styles.description}>
+						输入框在获取焦点时，会自动滚动到键盘之上。
+					</Text>
+					<TextInput 
+						placeholder='Text input' 
+						style={styles.input} 
+						underlineColorAndroid='transparent'
+					/>
+				</Dialog>
+			</View>
+		);
+	}
+}
+
 const styles = {
     showButton: {
     	margin: 10,
@@ -71,6 +113,27 @@ const styles = {
     okText: {
     	fontSize: 18,
     	color: '#2c8cff',
+    },
+    description: {
+    	margin: 20,
+    	fontSize: 16,
+    	color: 'rgba(0, 0, 0, 0.6)',
+    },
+    input: {
+    	marginHorizontal: 10,
+    	marginVertical: 50,
+    	paddingHorizontal: 7,
+    	paddingVertical: 4,
+    	borderWidth: 1,
+    	borderRadius: 4,
+    	height: 50,
+    	borderColor: '#49a9ee',
+    	shadowOffset: {width: 0, height: 0},
+    	shadowRadius: 2,
+    	shadowColor: 'rgba(16,142,233,.2)',
+    	shadowOpacity: 1,
+    	color: 'rgba(0,0,0,.65)',
+    	backgroundColor: '#fff',
     }
 };
 
@@ -80,6 +143,10 @@ exports.examples = [
 	{
 		title: 'Open a dialog',
 		render: () => <DialogExample />,
+	},
+	{
+		title: 'Scroll dialog to keyboard',
+		render: () => <ScrollToKeyboardExample />,
 	},
 ];
 
