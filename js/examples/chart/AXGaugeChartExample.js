42e71cb61bacc7cf8608f404c976f2944430e812
lvlin <lvlin@apexsoft.com.cn>
2017-03-16 16:42:27 +0800

    chore:更新仪表盘demo

--- js/examples/chart/AXGaugeChartExample.js
+++ js/examples/chart/AXGaugeChartExample.js
@@ -13,44 +13,21 @@ import ReactNative, {View, Text} from 'react-native';
 const {AXGaugeChart}=require('react-native-apex-charts');
 
 var parameterDef = {
-    // edgeInsets: {top: 0, left: 0, right: 0, bottom: 0},
     holeRadiusPercent:0.58,
-    maxAngle:360,
-    drawHoleEnabled:true,
-    // descriptionText:'服务覆盖率',
+    maxAngle:180,
     centerText: '30%',
     drawCenterTextEnabled: true,
 };
-
-var xaxisDef = {
-    // drawLabelsEnabled: true,
-    // labelFont: {
-    //     fontSize: 10
-    // },
-    // drawAxisLineEnabled: true,
-    // axisLineColor: 'white',
-    // axisLineLength: 20,
-    // axisLineWidth: 1,
-    labelCount: 11,
-    axisMinimum: 0,
-    axisMaximum: 100,
-    rotationAngle: 180,
-    maxAngle: 180,
-    gaugeColor: '#ff8257',
-    gaugeHoleRedius: 90,
-
-}
 
 class AXGaugeChartExample extends Component {
 
     render() {
 
 
-        return <View style={{height:210,width:200}}>
-            <AXGaugeChart style={{height:120,width:200}}
+        return <View style={{height:160,width:320}}>
+            <AXGaugeChart style={{flex:1,borderWidth:1,borderColor:'red'}}
                           onChartValueSelected={()=>{console.log("==>select")}}
                           parameter={{...parameterDef}}
-                          // xAxis={{...xaxisDef}}
                           data={{
                               dataSet:{
                                 yVals: [40],
