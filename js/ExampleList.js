0d77380263e58eefa758cf7cea7ac5dd2dfecb09
林金义 <linjinyi@apexsoft.com.cn>
2017-04-13 15:04:25 +0800

    add TimelineExample

--- js/ExampleList.js
+++ js/ExampleList.js
@@ -7,6 +7,7 @@ import {List} from 'react-native-apex-ui';
 const ListItem = List.ListItem;
 
 const Examples = [
+	require('./examples/TimelineExample'),
 	require('./examples/CollapseExample'),
 	require('./examples/ActionSheetExample'),
 	require('./examples/TooltipExample'),
